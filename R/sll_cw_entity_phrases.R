6debbdcc53162b9aea88c628da237e1069549fc8
cocteau <cocteau@stat.ucla.edu>
2013-11-17 18:21:57 -0500

    Update sll_cw_entity_phrases.R

--- R/sll_cw_entity_phrases.R
+++ R/sll_cw_entity_phrases.R
@@ -1,10 +1,11 @@
 sll_cw_entity_phrases = 
-function(entity,phrase=NULL,mincount=1,sort="count",
+function(entity,phrase=NULL,mincount=1,sort="count",state=NULL,
+         party=NULL,chamber=NULL,start_date=NULL,end_date=NULL,
   key = getOption("SunlightLabsKey", stop("need an API key for Sunlight Labs")))
 {
   url = paste("http://capitolwords.org/api/phrases/",entity,".json",sep="")
   page = 0
-  args <- compact(list(apikey=key, phrase=phrase,page=page))
+  args <- compact(list(apikey=key, phrase=phrase,page=page,state=state,party=party,chamber=chamber,start_date=start_date,end_date=end_date))
 
   data = NULL
 
@@ -21,7 +22,7 @@ function(entity,phrase=NULL,mincount=1,sort="count",
         data = rbind(data,datat)
      }
      page = page + 1
-     args <- compact(list(apikey=key, phrase=phrase,page=page))
+     args <- compact(list(apikey=key, phrase=phrase,page=page,state=state,party=party,chamber=chamber,start_date=start_date,end_date=end_date))
    }
      
   data
